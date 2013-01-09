69cf8eed8ad9d3afa6ae9ffe6b7ebd8e5b3079a1
杨博 <pop.atry@gmail.com>
2013-01-09 09:15:36 +0800

    Fix Int64/Float conversion

--- com/dongxiguo/hoo/Int64Helper.hx
+++ com/dongxiguo/hoo/Int64Helper.hx
@@ -12,7 +12,7 @@ extern class Int64Helper
 
   @:extern public static inline function floatToInt64(float:Float):Int64
   {
-    return Int64.make(Int32.ofInt(cast float / 4294967296.0), Int32.ofInt(cast float));
+    return Int64.make(Int32.ofInt(Math.floor(float / 4294967296.0)), Int32.ofInt(cast float));
   }
   
   @:extern public static inline function int64ToFloat(int64:Int64):Float
