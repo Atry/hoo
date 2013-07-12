e522688ce01b077c8f7e0b5d529ce270f12219df
杨博 <pop.atry@gmail.com>
2013-07-12 13:40:50 +0800

    Fix bugs when convert Int64 to Float.

--- com/dongxiguo/hoo/Int64Helper.hx
+++ com/dongxiguo/hoo/Int64Helper.hx
@@ -7,17 +7,23 @@ using haxe.Int32;
  * @author 杨博
  */
 @:final
-extern class Int64Helper 
+extern class Int64Helper
 {
 
   @:extern public static inline function floatToInt64(float:Float):Int64
   {
-    return Int64.make(Int32.ofInt(Math.floor(float / 4294967296.0)), Int32.ofInt(cast float));
+    return Int64.make(Int32.ofInt(Math.floor(float / 4294967296.0)), Int32.ofInt(Std.int(float)));
   }
-  
+
   @:extern public static inline function int64ToFloat(int64:Int64):Float
   {
-    return int64.getHigh().toNativeInt() * 4294967296.0 + int64.getLow().toNativeInt();
+    var high:Float = int64.getHigh().toNativeInt();
+    var low:Float = int64.getLow().toNativeInt();
+    if (low < 0)
+    {
+      high += 1;
+    }
+    return high * 4294967296.0 + low;
   }
-  
+
 }
